c1c0e677c7304dcccc868751f1fb0f0c45e4fa9f
pratham003 <46927163+pratham003@users.noreply.github.com>
2022-03-16 01:13:47 +0530

    Update index.php

--- MILESTONE 1/index.php
+++ MILESTONE 1/index.php
@@ -1,1 +1,5 @@
-
+Write a program to print  “Welcome to Girl Code It”?
+Write a program to print a maximum of two numbers?
+Write a program to check whether a number is even or odd?
+Write a program to check whether a number is negative, zero, positive?
+Write a program to find multiplication of two numbers?
