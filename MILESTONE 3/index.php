7fe58293060b79fbdb074d9da0b8e14a80e91837
pratham003 <46927163+pratham003@users.noreply.github.com>
2022-03-20 16:03:04 +0530

    Create index.php

---
+++ MILESTONE 3/index.php
@@ -0,0 +1,10 @@
+Write a C program to find first and last digit of a number.
+Write a C program to find sum of first and last digit of a number.
+Write a C program to swap first and last digits of a number.
+Write a C program to calculate sum of digits of a number.
+Write a C program to calculate product of digits of a number.
+Write a C program to enter a number and print its reverse.
+Write a C program to check whether a number is palindrome or not.
+Write a C program to find frequency of each digit in a given integer.
+Write a C program to enter a number and print it in words.
+Write a C program to print all ASCII character with their values.
