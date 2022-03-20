9b2ffe47a6193c6d418b0ec69889611fc56e6c9b
pratham003 <46927163+pratham003@users.noreply.github.com>
2022-03-20 16:01:16 +0530

    done milestone 2

--- MILESTONE 2/loops/index.php
+++ MILESTONE 2/loops/index.php
@@ -8,3 +8,5 @@ Write a C program to find sum of all even numbers between 1 to n.
 Write a C program to find sum of all odd numbers between 1 to n.
 Write a C program to print multiplication table of any number.
 Write a C program to count number of digits in a number.
+
+^_^
