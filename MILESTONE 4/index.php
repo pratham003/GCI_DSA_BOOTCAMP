d34496859d0e4076e9e14226f43d8432ccc2bdb3
pratham003 <46927163+pratham003@users.noreply.github.com>
2022-04-22 18:30:36 +0530

    Delete MILESTONE 4 directory

--- MILESTONE 4/index.php
+++
@@ -1,5 +0,0 @@
-Write a C program to find power of a number using for loop.
-Write a C program to find all factors of a number.
-Write a C program to calculate factorial of a number.
-Write a C program to find HCF (GCD) of two numbers.
-Write a C program to find LCM of two numbers.
